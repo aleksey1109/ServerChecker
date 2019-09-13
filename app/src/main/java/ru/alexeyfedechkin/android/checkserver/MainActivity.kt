48954a62f5d2fb09025e006a9fbcc6b55c5d6da4
fedechkin_alexey <aleksey-fedechkin@rambler.ru>
2019-09-13 17:13:03 +0700

    recreate project

---
+++ app/src/main/java/ru/alexeyfedechkin/android/checkserver/MainActivity.kt
@@ -0,0 +1,12 @@
+package ru.alexeyfedechkin.android.checkserver
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/main/java/ru/alexeyfedechkin/android/checkserver/Server.kt
@@ -0,0 +1,6 @@
+package ru.alexeyfedechkin.android.checkserver
+
+import io.realm.RealmObject
+
+class Server: RealmObject() {
+}
