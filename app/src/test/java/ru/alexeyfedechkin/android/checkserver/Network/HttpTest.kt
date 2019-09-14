8c0572bb570f05dbd2211c6c043d7434ef83b210
fedechkin_alexey <aleksey-fedechkin@rambler.ru>
2019-09-15 00:15:32 +0700

    add validatePort test

--- app/src/test/java/ru/alexeyfedechkin/android/checkserver/Network/HttpTest.kt
+++ app/src/test/java/ru/alexeyfedechkin/android/checkserver/Network/HttpTest.kt
@@ -11,4 +11,11 @@ class HttpTest {
         assertEquals(true, Http.validateResponseCode(200))
         assertEquals(false, Http.validateResponseCode(600))
     }
+
+    @Test
+    fun validatePort(){
+        assertEquals(true, Http.validatePort(100))
+        assertEquals(false, Http.validatePort(-1))
+        assertEquals(false, Http.validatePort(65536))
+    }
 }
